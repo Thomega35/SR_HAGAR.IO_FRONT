2ed16566f6d90d923f79a59b7864884193ce1194
Thomega35 <thomega35@gmail.com>
2024-02-13 13:38:14 +0100

    🧪 Add 100 players test

--- tests/multiUser.spec.ts
+++ tests/multiUser.spec.ts
@@ -27,3 +27,18 @@ test('Test 20 players', async ({ context }) => {
     await expect(page.getByRole('button', { name: 'Stop Game' })).toBeVisible();
   }
 });
+
+test('Test 100 players', async ({ context }) => {
+  test.setTimeout(120000);
+  for (let i = 0; i < 100; i++) {
+    // Create a page.
+    const page = await context.newPage(); 
+    await page.goto('https://sr.thomega.fr/');
+    await page.getByRole('button', { name: 'Start Game' }).click();
+    // move mouse to random position
+    await page.mouse.move(500, 500);
+    await page.mouse.move(200 + Math.random() * 500, 200 + Math.random() * 500);
+    await page.mouse.move(0 + Math.random() * 1000, 0 + Math.random() * 1000  );
+    await expect(page.getByRole('button', { name: 'Stop Game' })).toBeVisible();
+  }
+});
